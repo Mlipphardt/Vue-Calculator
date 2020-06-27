da3af7a6b97c73c81123e230694352fb9c4fd3f2
Mlipphardt <matthewlipphardt@gmail.com>
2020-06-27 17:23:19 -0400

    Loop through values and operators to create buttons

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 var calculator = new Vue({
   el: "#calculator",
-  data: { num1: null, num2: null, operator: null },
+  data: {
+    num1: null,
+    num2: null,
+    operator: null,
+    values: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    operators: ["+", "-", "x", "/"],
+  },
   methods: {
     calculate: function () {
       switch (this.operator) {
-          case "plus":
-              console.log(num1 + num2);
-              break;
-          case "minus":
-              console.log(num1 - num2);
-              break;
-          case "times":
-              console.log(num1 * num2);
-              break;
-          case "divide":
-              console.log(num1 / num2);
-              break
-          case:
-            console.log("ERR")
+        case "plus":
+          console.log(num1 + num2);
+          break;
+        case "minus":
+          console.log(num1 - num2);
+          break;
+        case "times":
+          console.log(num1 * num2);
+          break;
+        case "divide":
+          console.log(num1 / num2);
+          break;
+        default:
+          console.log("ERR");
       }
     },
   },
