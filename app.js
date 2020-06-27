0c9854e9a7df88c8e0f544fe0bcba5925f0f4226
Mlipphardt <matthewlipphardt@gmail.com>
2020-06-27 17:50:13 -0400

    Basic calculator functionality working

--- app.js
+++ app.js
@@ -3,32 +3,52 @@ var calculator = new Vue({
   data: {
     num1: null,
     num2: null,
-    operator: null,
+    chosenOperator: "",
     values: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    operators: ["+", "-", "x", "/"],
+    operators: ["plus", "minus", "times", "divide"],
   },
   methods: {
     calculate: function () {
-      switch (this.operator) {
+      switch (this.chosenOperator) {
         case "plus":
-          console.log(num1 + num2);
+          console.log(this.num1 + this.num2);
+          this.clear();
           break;
         case "minus":
-          console.log(num1 - num2);
+          console.log(this.num1 - this.num2);
+          this.clear();
           break;
         case "times":
-          console.log(num1 * num2);
+          console.log(this.num1 * this.num2);
+          this.clear();
           break;
         case "divide":
-          console.log(num1 / num2);
+          console.log(this.num1 / this.num2);
+          this.clear();
           break;
         default:
           console.log("ERR");
       }
     },
+    clear: function () {
+      (this.chosenOperator = ""), (this.num1 = null), (this.num2 = null);
+    },
     printNumber: function (number) {
       console.log("Printing a number!");
       console.log(number);
+      if (this.chosenOperator == "") {
+        this.num1 = number;
+        console.log("First number set?");
+      } else {
+        this.num2 = number;
+        console.log("Second number set ?");
+      }
+    },
+    setOperator: function (operator) {
+      console.log(operator);
+      console.log(this.chosenOperator);
+      this.chosenOperator = operator;
+      console.log(this.chosenOperator);
     },
   },
 });
